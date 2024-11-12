078013db4afabb401b693ffea2ff834a7a8d07bb
Yatma Gueye <gueye.yatma@gmail.com>
2024-11-12 11:46:16 +0000

    Nettoyages et enrichissements

---
+++ models/staging/gz_raw_data/stg_gz_raw_data__navire_gz_brut.sql
@@ -0,0 +1,22 @@
+with 
+
+source as (
+
+    select * from {{ source('gz_raw_data', 'navire_gz_brut') }}
+
+),
+
+renamed as (
+
+    select
+        orders_id,
+        shipping_fee,
+        shipping_fee_1,
+        logcost,
+        ship_cost
+        cast(ship_cost as FLOAT64) as ship_cost  -- Conversion de ship_cost en FLOAT64
+    from source
+    where shipping_fee <> shipping_fee_1  -- Filtrage des lignes où shipping_fee est différent de shipping_fee_1
+)
+
+select * from renamed
