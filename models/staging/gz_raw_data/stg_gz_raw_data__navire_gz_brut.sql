8bc14f5cd8586ce71aeff1770768b70067a14793
Yatma Gueye <gueye.yatma@gmail.com>
2024-11-12 13:20:32 +0000

    Derniers modifs sur ship

--- models/staging/gz_raw_data/stg_gz_raw_data__navire_gz_brut.sql
+++ models/staging/gz_raw_data/stg_gz_raw_data__navire_gz_brut.sql
@@ -1,21 +1,16 @@
 with 
 
 source as (
-
     select * from {{ source('gz_raw_data', 'navire_gz_brut') }}
-
 ),
 
 renamed as (
-
     select
         orders_id,
         shipping_fee,
         logcost,
-        ship_cost
-        cast(ship_cost as FLOAT64) as ship_cost  -- Conversion de ship_cost en FLOAT64
+        CAST(ship_cost AS INT) AS ship_cost  -- Utilise safe_cast pour éviter les erreurs de conversion
     from source
-    where shipping_fee <> shipping_fee_1  -- Filtrage des lignes où shipping_fee est différent de shipping_fee_1
 )
 
 select * from renamed
